265cef6ba16a1d6cbfb28ef448a96966fe77a434
Preston <preston.ip@abingdon.org.uk>
2026-08-05 16:15:00 +0800

    Finished JavaScript game

--- test.js
+++ test.js
@@ -1,15 +1,3 @@
-function foo(a, b) {
-    return a * b
-}
-
-function dispFoo(a, b) {
-    console.log(foo(a,b))
-}
-
-console.log("Hello World")
-console.log(foo(2,4,2020,330))
-// dispFoo.bind(this,2,3)()
-setTimeout(console.log.bind(this, foo(2,3)), 5000)
-
-// console.log.bind(6)()
-new Promise().then
+let x = 0
+for (let i = 0; i < 9; i++) {x++}
+console.log(x)
