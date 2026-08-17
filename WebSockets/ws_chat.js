dcd82c396459fefcd9351961c2b1965a3513a8e3
Preston <preston.ip@abingdon.org.uk>
2026-08-17 16:50:21 +0800

    Chat app frontend HTML

--- WebSockets/ws_chat.js
+++ WebSockets/ws_chat.js
@@ -1,23 +1,45 @@
-// const ws = require("ws")
-// import ws from "ws"
+// TODO: Usernames, backend server
 
-const wss = new WebSocket("ws://localhost:8080")
+let wss = new WebSocket("ws://localhost:8080")
+let connected = false
 
-document.getElementById("text").innerText = "wahs"
-// document.getElementById("textbox").addEventListener('click', function () {
-//     document.getElementById("text").innerText = "Clicked!"
-// })
-document.getElementById("form").addEventListener('submit', function (event) {
-    event.preventDefault()
-    const msg = document.getElementById("textbox").value
-    wss.send(msg)
+document.getElementById("form").addEventListener('submit', sendMessage)
+
+document.getElementById("form").addEventListener('keydown', function(event) {
+    if (event.key == 'Enter' && !event.shiftKey) sendMessage(event)
 })
-// document.getElementById("submit").addEventListener('click', function () {
-//     const msg = document.getElementById("textbox").value
-//     wss.send(msg)
-// })
 
+function sendMessage(event) {
+    event.preventDefault()
+    if (connected) {
+        const msgbox = document.getElementById("textbox")
+        const msg = msgbox.value.trim()
+        if (msg) {
+            wss.send(msgbox.value)
+            msgbox.value = ""
+        }
+    }
+}
 
-wss.onmessage = (msg) => {
+function connClosed() {
+    document.getElementById("connecting").style.color = "red"
+    document.getElementById("connText").innerText = "Disconnected"
+    connected = false
+}
+
+function connOpened() {
+    document.getElementById("connecting").style.color = "lawngreen"
+    document.getElementById("connText").innerText = "Connected"
+    connected = true
+}
+
+// TODO: Fit msg {user: XXX, content: XXX, time: XXX} format
+function connMessage(msg) {
     document.getElementById("text").innerText += "\n"+msg.data
 }
+
+
+wss.onmessage = connMessage
+wss.onclose = connClosed
+wss.onerror = connClosed
+wss.onopen = connOpened
